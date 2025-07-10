cf8cd4264da22bd074a3a674d085bb1e33061497
Saiful Islam <sumon.sis169@gmail.com>
2025-07-10 10:13:59 +0600

    all layout done

---
+++ src/AllRoute/Router/Routes.jsx
@@ -0,0 +1,43 @@
+import LogIn from '@/Authentication/LogIn/LogIn';
+import Register from '@/Authentication/Register/Register';
+import AuthLayout from '@/Layout/AuthLayout/AuthLayout';
+import DashboardLayout from '@/Layout/DashboardLayout/DashboardLayout';
+import MainLayout from '@/Layout/MainLayout/MainLayout';
+import Home from '@/Pages/Home/Home';
+import { createBrowserRouter } from 'react-router';
+
+export const Routes = createBrowserRouter([
+  {
+    path: '/',
+    Component: MainLayout,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+    ],
+  },
+  {
+    path: '/',
+    Component: AuthLayout,
+    children: [
+      {
+        path: 'login',
+        Component: LogIn,
+      },
+      {
+        path: 'register',
+        Component: Register,
+      },
+    ],
+  },
+  {
+    path: '/dashboard',
+    element: <DashboardLayout />,
+    children: [
+      {
+        index: true,
+      },
+    ],
+  },
+]);
